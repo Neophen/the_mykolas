34bb08cc1221839a9954d75a3780118da00d887d
Mykolas Mankevcius <mykolas@unity3d.com>
2020-08-25 22:05:44 +0300

    add postcss

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,8 +1,21 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+      './js/app.js',
+      "../lib/**/*.ex",
+      "../lib/**/*.eex",
+      "../lib/**/*.leex",
+    ],
+
+    defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+  });
+
+
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
-    require('postcss-nested')
+    require('postcss-nested'),
+    ...(process.env.NODE_ENV == 'production' ? [purgecss] : []),
   ]
 };
